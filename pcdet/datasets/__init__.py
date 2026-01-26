a213a55fdfafcc1189bd6f25593e0e0370f9234d
Jinsong Li <44409123+LeeXiaosong@users.noreply.github.com>
2026-01-27 01:20:42 +0800

    Add files via upload

---
+++ pcdet/datasets/__init__.py
@@ -0,0 +1,90 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import DistributedSampler as _DistributedSampler
+
+from pcdet.utils import common_utils
+
+from .dataset import DatasetTemplate
+from .kitti.kitti_dataset import KittiDataset
+from .nuscenes.nuscenes_dataset import NuScenesDataset,NuScenesDatasetD6
+# from .waymo.waymo_dataset import WaymoDataset
+
+__all__ = {
+    'DatasetTemplate': DatasetTemplate,
+    'KittiDataset': KittiDataset,
+    'NuScenesDataset': NuScenesDataset,
+    'NuScenesDatasetD6': NuScenesDatasetD6,
+    # 'WaymoDataset': WaymoDataset
+}
+
+
+class DistributedSampler(_DistributedSampler):
+
+    def __init__(self, dataset, num_replicas=None, rank=None, shuffle=True):
+        super().__init__(dataset, num_replicas=num_replicas, rank=rank)
+        self.shuffle = shuffle
+
+    def __iter__(self):
+        if self.shuffle:
+            g = torch.Generator()
+            g.manual_seed(self.epoch)
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+        else:
+            indices = torch.arange(len(self.dataset)).tolist()
+
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
+        return iter(indices)
+
+
+def build_dataloader(dataset_cfg, class_names, batch_size, dist, root_path=None, workers=4,
+                     logger=None, training=True, merge_all_iters_to_one_epoch=False, total_epochs=0):
+    """
+    构建数据集并调用DataLoader进行加载
+    Args:
+        dataset_cfg: 数据集配置文件
+        class_names: 类比名称
+        batch_size: batch的大小
+        dist: 是否并行训练
+        root_path: 根目录
+        workers: 线程数
+        logger: 日志记录器
+        training: 训练模式
+        merge_all_iters_to_one_epoch: 是否将所有迭代次数合并到一个epoch
+        total_epochs: 总epoch数
+    Returns
+        dataset: 数据集
+        dataloader: 数据加载器
+        sampler: 数据采样器
+    """
+    dataset = __all__[dataset_cfg.DATASET]( # DatasetTemplate
+        dataset_cfg = dataset_cfg, # _BASE_CONFIG_: ./tools/cfgs/dataset_configs/nuscenes_dataset.yaml
+        class_names = class_names, 
+        root_path = root_path,
+        training = training,
+        logger = logger,
+    )
+
+    if merge_all_iters_to_one_epoch:
+        assert hasattr(dataset, 'merge_all_iters_to_one_epoch')
+        dataset.merge_all_iters_to_one_epoch(merge=True, epochs=total_epochs)
+
+    if dist:
+        if training:
+            sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        else:
+            rank, world_size = common_utils.get_dist_info()
+            sampler = DistributedSampler(dataset, world_size, rank, shuffle=False)
+    else:
+        sampler = None
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, pin_memory=True, num_workers=workers,
+        shuffle=(sampler is None) and training, collate_fn=dataset.collate_batch,
+        drop_last=False, sampler=sampler, timeout=0
+    )
+
+    return dataset, dataloader, sampler
---
+++ pcdet/datasets/augmentor/augmentor_utils.py
@@ -0,0 +1,145 @@
+import numpy as np
+import cv2
+
+from ...utils import common_utils
+
+
+def get_rotation_scale_matrix2d(center, angle, scale):
+    """
+    :param center: [x, y]
+    :param angle: angle in radians, >0: clockwise
+    :param scale: >1 enlarge
+    :return:
+    """
+    alpha = scale * np.cos(-angle)
+    beta = scale * np.sin(-angle)
+    return np.array([[alpha, beta, center[0] - beta * center[1]],
+                     [-beta, alpha, beta * center[0] + (1 - alpha) * center[1]]])
+
+
+def global_rotation_voxel_feat(voxel_feat, theta, scale=1.0):
+    ny, nx, c = voxel_feat.shape  # H W C
+    # rot_mat = get_rotation_scale_matrix2d((int(nx / 2), int(ny / 2)), theta, scale)
+    rot_mat = cv2.getRotationMatrix2D((int(nx / 2), int(ny / 2)), -theta * 180 / np.pi, scale)
+    voxel_feat = cv2.warpAffine(voxel_feat, rot_mat, (nx, ny), flags=cv2.INTER_NEAREST)  # H W C
+    return voxel_feat.reshape(ny, nx, -1)
+
+
+def global_scale_voxel_feat(voxel_feat, scale, theta=0.0):
+    ny, nx, c = voxel_feat.shape  # H W C
+    # rot_mat = get_rotation_scale_matrix2d((int(nx / 2), int(ny / 2)), theta, scale)
+    rot_mat = cv2.getRotationMatrix2D((int(nx / 2), int(ny / 2)), -theta * 180 / np.pi, scale)
+    voxel_feat = cv2.warpAffine(voxel_feat, rot_mat, (nx, ny), flags=cv2.INTER_NEAREST)  # H W C
+    return voxel_feat.reshape(ny, nx, -1)
+
+
+def random_flip_along_x(gt_seg, points, observations, binarymap, reflections, transmissions):
+    """
+    Args:
+        gt_seg: 500, 1000, 1
+        points: (M, 3 + C)
+    Returns:
+    """
+    enable = np.random.choice([False, True], replace=False, p=[0.5, 0.5])
+    if enable:
+        points[:, 1] = -points[:, 1]
+        gt_seg = np.flipud(gt_seg)
+        if observations is not None:
+            observations = np.flipud(observations)
+            binarymap = np.flipud(binarymap)
+            reflections = np.flipud(reflections)
+            transmissions = np.flipud(transmissions)
+    return gt_seg, points, observations, binarymap, reflections, transmissions
+
+
+def random_flip_along_y(gt_seg, points, observations, binarymap, reflections, transmissions):
+    """
+    Args:
+        gt_seg: 500, 1000, 1
+        points: (M, 3 + C)
+    Returns:
+    """
+    enable = np.random.choice([False, True], replace=False, p=[0.5, 0.5])
+    if enable:
+        points[:, 0] = -points[:, 0]
+        gt_seg = np.fliplr(gt_seg)
+        if observations is not None:
+            observations = np.fliplr(observations)
+            binarymap = np.fliplr(binarymap)
+            reflections = np.fliplr(reflections)
+            transmissions = np.fliplr(transmissions)
+    return gt_seg, points, observations, binarymap, reflections, transmissions
+
+
+def global_rotation(gt_seg, points, observations, binarymap, reflections, transmissions, rot_range):
+    """
+    Args:
+        gt_boxes: (N, 7 + C), [x, y, z, dx, dy, dz, heading, [vx], [vy]]
+        points: (M, 3 + C),
+        rot_range: [min, max]
+    Returns:
+    """
+    noise_rotation = np.random.uniform(rot_range[0], rot_range[1])
+    points = common_utils.rotate_points_along_z(points[np.newaxis, :, :], np.array([noise_rotation]))[0]
+    gt_seg = global_rotation_voxel_feat(gt_seg, noise_rotation)
+    if observations is not None:
+        observations = global_rotation_voxel_feat(observations, noise_rotation)
+        binarymap = global_rotation_voxel_feat(binarymap, noise_rotation)
+        reflections = global_rotation_voxel_feat(reflections, noise_rotation)
+        transmissions = global_rotation_voxel_feat(transmissions, noise_rotation)
+    return gt_seg, points, observations, binarymap, reflections, transmissions
+
+
+def global_scaling(gt_seg, points, observations, binarymap, reflections, transmissions, scale_range):
+    """
+    Args:
+        gt_boxes: (N, 7), [x, y, z, dx, dy, dz, heading]
+        points: (M, 3 + C),
+        scale_range: [min, max]
+    Returns:
+    """
+    if scale_range[1] - scale_range[0] < 1e-3:
+        return gt_seg, points, observations, binarymap, reflections, transmissions
+    noise_scale = np.random.uniform(scale_range[0], scale_range[1])
+    points[:, :3] *= noise_scale
+    gt_seg = global_scale_voxel_feat(gt_seg, noise_scale)
+    if observations is not None:
+        observations = global_scale_voxel_feat(observations, noise_scale)
+        binarymap = global_scale_voxel_feat(binarymap, noise_scale)
+        reflections = global_scale_voxel_feat(reflections, noise_scale)
+        transmissions = global_scale_voxel_feat(transmissions, noise_scale)
+    return gt_seg, points, observations, binarymap, reflections, transmissions
+
+
+def global_translate_voxel_feat(voxel_feat, dw, dh):
+    ny, nx, c = voxel_feat.shape  # H W C
+    mat = np.array([[1, 0, dw], [0, 1, dh]], dtype=np.float32)
+    voxel_feat = cv2.warpAffine(voxel_feat, mat, (nx, ny), flags=cv2.INTER_NEAREST)  # H W C
+    return voxel_feat.reshape(ny, nx, -1)
+
+
+def global_translate(gt_seg, points, observations, binarymap, reflections, transmissions, noise_translate_std):
+    """
+    Apply global translation to gt_boxes and points.
+    """
+    if not isinstance(noise_translate_std, (list, tuple, np.ndarray)):
+        noise_translate_std = np.array([noise_translate_std, noise_translate_std, noise_translate_std])
+
+    std_x, std_y, std_z = noise_translate_std
+    noise_translate = np.array([np.random.normal(0, std_x, 1),
+                                np.random.normal(0, std_y, 1),
+                                np.random.normal(0, std_z, 1)]).T  # 1 3
+    # clip to 3*std
+    noise_translate = np.clip(noise_translate, [-3.0*std_x, -3.0*std_y, -3.0*std_z], [3.0*std_x, 3.0*std_y, 3.0*std_z])
+    points[:, :3] += noise_translate
+
+    dw = noise_translate[0, 0] // 0.1
+    dh = noise_translate[0, 1] // 0.1
+    gt_seg = global_translate_voxel_feat(gt_seg, dw, dh)
+    if observations is not None:
+        observations = global_translate_voxel_feat(observations, dw, dh)
+        binarymap = global_translate_voxel_feat(binarymap, dw, dh)
+        reflections = global_translate_voxel_feat(reflections, dw, dh)
+        transmissions = global_translate_voxel_feat(transmissions, dw, dh)
+
+    return gt_seg, points, observations, binarymap, reflections, transmissions
