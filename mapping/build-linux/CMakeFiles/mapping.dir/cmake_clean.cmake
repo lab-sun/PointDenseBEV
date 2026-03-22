8d969ab5cb7c2d93d90ba6a97990fe307e84d21d
Jinsong Li <44409123+LeeXiaosong@users.noreply.github.com>
2026-03-23 01:07:58 +0800

    Add files via upload

---
+++ mapping/build-linux/CMakeFiles/mapping.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/leon/Documents/Projects/MASS/mapping/mapping.cpython-37m-x86_64-linux-gnu.so"
+  "/home/leon/Documents/Projects/MASS/mapping/mapping.pdb"
+  "CMakeFiles/mapping.dir/mapping.cpp.o"
+  "CMakeFiles/mapping.dir/mapping.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
