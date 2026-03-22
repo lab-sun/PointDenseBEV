d856c10c7d767afdbede58ea1c4b30d5e8b0b5e3
Jinsong Li <44409123+LeeXiaosong@users.noreply.github.com>
2026-03-23 01:04:09 +0800

    Add files via upload

---
+++ voxelize/CMakeFiles/dense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dense.dir/dense.cpp.o"
+  "dense.cpython-37m-x86_64-linux-gnu.so"
+  "dense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
